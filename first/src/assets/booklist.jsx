f6edc3b3c71870419f55f67aaccc05219e0e2f43
Sravanikavala <kavalasravani@gmail.com>
2024-02-28 20:56:37 +0530

    git demo

---
+++ first/src/assets/booklist.jsx
@@ -0,0 +1,10 @@
+function booklist()
+{
+return(
+    <div>
+        <h2> hello </h2>
+        <p> it is a react course </p>
+    </div>
+)
+}
+export default booklist
---
+++ first/src/assets/booklistclass.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Component } from "react";
+
+class booklistclass extends Component
+{
+    render()
+    {
+        return(
+            <div>
+                <h2>hello </h2>
+                <p>it is a react course </p>
+            </div>
+        )
+    }
+}
+export default booklistclass
